89cde2147e77b5db5f8d41cbe950afac5cf2b969
Dominika Pawlaczyk <pawlaczyk@g.pl>
2016-03-27 00:23:55 +0100

    strona glowna + root + user bez wyciagniecia danych z input html

--- httpServer.py
+++ httpServer.py
@@ -1,12 +1,37 @@
 #!/usr/bin/python
+#coding=utf-8
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 from os import curdir, sep
+import cgi
+import os
+import subprocess
+
+def php(script_path):
+    p = subprocess.Popen(['php', script_path] )
+
 
 PORT_NUMBER = 8080
 
 #This class will handles any incoming request from
 #the browser 
 class myHandler(BaseHTTPRequestHandler):
+	
+	def _set_headers(self):
+		self.send_response(200)
+		self.send_header('Content-type', 'text/html')
+		self.end_headers()
+	
+	def do_POST(self):
+		php("user.php")
+		print "POST METHOD"
+		self._set_headers()
+		self.wfile.write("""
+		<form name="main" action="index.html" method="post" accept-charset="utf-8">  
+    		<button type= submit> Powrót na główną stronę </button>  
+			</form>
+		
+		""")
+
 	
 	#Handler for the GET requests
 	def do_GET(self):
@@ -43,6 +68,8 @@ class myHandler(BaseHTTPRequestHandler):
 				self.wfile.write(f.read())
 				f.close()
 			return
+			
+
 
 
 		except IOError:
---
+++ user.php
@@ -0,0 +1,5 @@
+<?php
+
+$password = (isset($_POST['password']) ? $_POST['password'] : null);
+echo $password;
+?>
